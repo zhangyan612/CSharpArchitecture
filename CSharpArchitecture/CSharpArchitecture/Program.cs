7b595eee010d750d2d0de161d384b1efe035cf75
Yan Zhang <zhangyan612@gmail.com>
2018-05-12 12:24:06 -0500

    initial commit of base project

---
+++ CSharpArchitecture/CSharpArchitecture/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpArchitecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
